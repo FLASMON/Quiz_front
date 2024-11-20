3286dc25accaa323e1026113eda4f6c6cc16dae1
FLASMON <flasmonjame53@gmail.com>
2024-11-20 15:01:44 +0300

    ajouter bottom commencez

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import App from './App.vue'
 import 'ant-design-vue/dist/reset.css';
 import Antdv from "ant-design-vue";
 
+import './Icons/icons.js'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
 // importing AOS
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 const app = createApp(App);
-
+app.component("font-awesome-icon", FontAwesomeIcon)
 app.use(AOS.init());
 app.use(Antdv)
 app.mount('#app')
